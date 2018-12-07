3b982ab9c221c876d7ca8f296215521c2cb9aea7
Hirmu <sirsipsihirmu@gmail.com>
2018-12-07 18:49:35 +0200

    Muutettu testcaseja

---
+++ ArmourTester/ArmourTester.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing2018;
+
+namespace ArmourTester
+{
+    [TestClass]
+    public class ArmourTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+            int lvl = ar.getLevel();
+
+            if (lvl != 2)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+            int slot = ar.getSlot();
+
+            Assert.IsTrue(slot == 2);
+            
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+            int maxProt = ar.getMaxProt();
+            int curProt = ar.getCurProt();
+
+            if (curProt / maxProt == 20)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+            int lvl = ar.getLevel();
+
+            Assert.AreEqual(lvl, 2);
+            
+        }
+
+    }
+}
---
+++ ArmourTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArmourTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArmourTester")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2735d7de-9600-4a64-ac0c-675a4374b893")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
